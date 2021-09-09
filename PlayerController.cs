79c6fb03d68b51bc34f992f9c9a16267154a7781
shuichi <32384416+mrgarita@users.noreply.github.com>
2021-09-09 09:54:20 +0900

    for Player move range check

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,22 +8,46 @@ public class PlayerController : MonoBehaviour
     public float Speed;
     // 弾のプレハブ
     public GameObject bulletPrefab;
+    // プレイヤーの位置
+    Vector3  p;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // プレイヤーの初期位置を設定
+        p= new Vector3(transform.position.x, transform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 方向キーで入力された横軸の値を取得
+        // ゲームパッド・方向キーで入力された値を取得
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
  
-        // 現在位置にx,y の値を加算する
-        transform.position += new Vector3(x, y, 0) * Time.deltaTime * Speed;
+        // 現在位置に入力値（x,y）を加算する
+        p += new Vector3(x, y, 0) * Time.deltaTime * Speed;
+
+        // プレイヤーの移動範囲を設定
+        if(p.x > 2.7f)
+        {
+            p.x = 2.7f;
+        }
+        else if(p.x < -2.7f)
+        {
+            p.x = -2.7f;
+        }
+        if (p.y > 4.8f)
+        {
+            p.y = 4.8f;
+        }
+        else if (p.y < -4.8f)
+        {
+            p.y = -4.8f;
+        }
+
+        // プレイヤーの位置を決定
+        transform.position = p;
 
         // ジョイパッドのAボタン　または　zキーが押されたら弾を発射
         if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("z"))

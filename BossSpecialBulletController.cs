5a51ca0803a210d3074b5ad7de3e47d88aa94e34
shuichi <32384416+mrgarita@users.noreply.github.com>
2021-10-29 11:33:22 +0900

    ボスの強化攻撃（よけるのが難しい速いショット）を追加する

---
+++ BossSpecialBulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpecialBulletController : MonoBehaviour
+{
+    // 弾の速度
+    public float Speed;
+    // 発射音
+    public AudioClip Se;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 起動時に発射音を鳴らす
+        AudioSource.PlayClipAtPoint(Se, new Vector3(0, 0, -10f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 弾を移動する（下方向）
+        transform.position += Vector3.down * Speed * Time.deltaTime;
+
+        // 画面下に消えたら弾を削除
+        if (transform.position.y < -5.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

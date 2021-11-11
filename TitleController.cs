00c9e9fae0a7a9e7679a7229b40ec5cf1ae1c487
shuichi <32384416+mrgarita@users.noreply.github.com>
2021-11-11 10:13:51 +0900

    タイトル画面からゲーム画面への遷移

---
+++ TitleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // ジョイパッドのBボタン　または　Enterキーが押されたらゲーム開始
+    if (Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.Return))
+    {
+      SceneManager.LoadScene("GameScene");
+    }
+  }
+}
